c5d0029b0c3e0877cc786a1e42ac4bb5351a24f5
Liran Dekamhi <liran.kim@gmail.com>
2026-05-26 19:04:38 +0300

    Set up app routes for login and tasks components

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'tasks', pathMatch: 'full' },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/login.component').then(m => m.LoginComponent),
+  },
+  {
+    path: 'tasks',
+    loadComponent: () =>
+      import('./features/tasks/task-list.component').then(m => m.TaskListComponent),
+    canActivate: [authGuard],
+  },
+  { path: '**', redirectTo: 'tasks' },
+];
